ed9958219905650ca0dce1cd00853bd323b8b499
Dashuai.Quan <Dashuai.Quan@outlook.com>
2021-10-08 08:22:21 +0800

    Revert "[skip ci] add tests for hash table"
    This reverts commit ded6d598c0e449959ec3434bdbe14561e6545603.

--- test/container/hash_table_test.cpp
+++ test/container/hash_table_test.cpp
@@ -21,16 +21,10 @@
 
 namespace bustub {
 
-// Only for test
-class FakeHash : public HashFunction<int> {
- public:
-  virtual uint64_t GetHash(int key) override {
-    return key;
-  }
-};
+// NOLINTNEXTLINE
 
 // NOLINTNEXTLINE
-TEST(HashTableTest, SampleTest) {
+TEST(HashTableTest, DISABLED_SampleTest) {
   auto *disk_manager = new DiskManager("test.db");
   auto *bpm = new BufferPoolManagerInstance(50, disk_manager);
   ExtendibleHashTable<int, int, IntComparator> ht("blah", bpm, IntComparator(), HashFunction<int>());
@@ -116,66 +110,6 @@ TEST(HashTableTest, SampleTest) {
   }
 
   ht.VerifyIntegrity();
-
-  disk_manager->ShutDown();
-  remove("test.db");
-  delete disk_manager;
-  delete bpm;
-}
-
-// NOLINTNEXTLINE
-TEST(HashTableTest, HardTest) {
-  auto *disk_manager = new DiskManager("test.db");
-  auto *bpm = new BufferPoolManagerInstance(50, disk_manager);
-  ExtendibleHashTable<int, int, IntComparator> ht("blah", bpm, IntComparator(), FakeHash());
-
-  int bucket_array_size = (4 * PAGE_SIZE) / (4 * sizeof(std::pair<int, int>) + 1);
-
-  // make a bucket full
-  for (int i = 0; i < bucket_array_size; i++) {
-    ht.Insert(nullptr, i * 2, i * 2);
-    std::vector<int> res;
-    ht.GetValue(nullptr, i * 2, &res);
-    EXPECT_EQ(1, res.size()) << "Failed to insert " << i * 2 << std::endl;
-    EXPECT_EQ(i * 2, res[0]);
-  }
-
-  EXPECT_EQ(1, ht.GetGlobalDepth());
-
-  ht.VerifyIntegrity();
-
-  // insert a few keys into another bucket
-  for (int i = 1; i < 5; i++) {
-    ht.Insert(nullptr, i + 2, i + 2);
-    std::vector<int> res;
-    ht.GetValue(nullptr, i + 2, &res);
-    EXPECT_EQ(1, res.size()) << "Failed to insert " << i + 2 << std::endl;
-    EXPECT_EQ(i + 2, res[0]);
-  }
-
-  EXPECT_EQ(1, ht.GetGlobalDepth());
-
-  ht.VerifyIntegrity();
-
-  // add a key to overflow the first bucket
-  ht.Insert(nullptr, bucket_array_size * 2, bucket_array_size * 2);
-
-  EXPECT_EQ(2, ht.GetGlobalDepth());
-
-  ht.VerifyIntegrity();
-
-  // remove all keys of the first bucket and its split image
-  for (int i = 0; i <= bucket_array_size; i++) {
-    ht.Remove(nullptr, i * 2, i * 2);
-  }
-
-  EXPECT_EQ(1, ht.GetGlobalDepth());
-
-  ht.VerifyIntegrity();
-
-  for (int i = 1; i < 5; i++) {
-    ht.Remove(nullptr, i + 2, i + 2);
-  }
 
   disk_manager->ShutDown();
   remove("test.db");
